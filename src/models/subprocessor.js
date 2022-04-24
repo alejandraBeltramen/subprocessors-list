d9843d1d683d949c0c0dc1eb02eb4eade5c2d587
Alejandra Beltramen <alejandra.beltramen@gmail.com>
2022-04-24 13:50:59 -0300

    Created a Subprocessor class

---
+++ src/models/subprocessor.js
@@ -0,0 +1,8 @@
+export default class Subprocessor {
+    constructor(name, purpose, location) {
+        this.id = Math.random();
+        this.name = name;
+        this.purpose = purpose;
+        this.location = location;
+    }
+}
