ce341f19a041645b16c59ddbd9f5494a603585b8
Alejandra Beltramen <alejandra.beltramen@gmail.com>
2022-04-24 13:48:29 -0300

    Customized MUI table

---
+++ src/components/UI/Table.js
@@ -0,0 +1,24 @@
+import MUITableCell, { tableCellClasses } from '@mui/material/TableCell';
+import MUITableRow from '@mui/material/TableRow';
+import { styled } from '@mui/material/styles';
+import { pxToRem } from '../../theme/utils';
+
+export const TableRow = styled(MUITableRow)(() => ({
+    // hide last border
+    '&:last-child td': {
+      border: 0,
+    },
+}));
+
+export const TableCell = styled(MUITableCell)(() => ({
+    [`&.${tableCellClasses.head}`]: {
+        padding: pxToRem(16),
+        fontWeight: 700,
+        padding: pxToRem(8),
+    },
+    [`&.${tableCellClasses.body}`]: {
+        padding: pxToRem(16),
+        padding: pxToRem(8),
+        borderBottom: '1px solid rgb(247 245 245)',
+    },
+}));
